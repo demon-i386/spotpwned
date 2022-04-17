08d73dc67696bdf56f643c5c5ed5273378514aeb
halaxysounds <halaxysounds@gmail.com>
2022-04-17 17:43:01 -0300

    switch from sounddevice to pydub, fix audio playback

--- spotpwned.py
+++ spotpwned.py
@@ -1,6 +1,8 @@
 from curses import raw
 import requests
-import sounddevice as sd
+from pydub import AudioSegment
+from pydub.playback import play
+import io
 
 unauth_server_certificate_base = 'https://spclient.wg.spotify.com'
 certificate_common_name = ['widevine', 'playready', 'fairplay', 'spotify-invalid']
@@ -8,7 +10,6 @@ certificate_common_name = ['widevine', 'playready', 'fairplay', 'spotify-invalid
 def get_widevine_certificate():
        application_cert = requests.get(f"{unauth_server_certificate_base}/{certificate_common_name[0]}-license/v1/application-certificate").content
        return application_cert
-
 
 def initiate_content_recovery():
        headers = {}
@@ -18,9 +19,9 @@ def initiate_content_recovery():
               print(f"Content bytes :: {raw_bytes.headers['Content-Range']}")
        except:
               print('Could not find content range (not sent in header response?)')
-       print(raw_bytes.content)
-       sd.play(raw_bytes.content, 44100)
-
-print(f"G0t certificate :: {get_widevine_certificate()}" + ('\n'*3))
+       audio = AudioSegment.from_file(io.BytesIO(raw_bytes.content), format='raw', frame_rate=44100, channels=2, sample_width=2).remove_dc_offset()
+       # reduce volume as random data could damage speakers
+       quieter = audio - 3.5
+       play(quieter)
 
 initiate_content_recovery()
