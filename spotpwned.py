2dac585589abedd5b86fd6d940d9c40dd5f7ee11
halaxysounds <halaxysounds@gmail.com>
2022-04-17 17:08:05 -0300

    add try catch on printing content range header

--- spotpwned.py
+++ spotpwned.py
@@ -1,9 +1,9 @@
+from curses import raw
 import requests
 import sounddevice as sd
 
 unauth_server_certificate_base = 'https://spclient.wg.spotify.com'
 certificate_common_name = ['widevine', 'playready', 'fairplay', 'spotify-invalid']
-
 
 def get_widevine_certificate():
        application_cert = requests.get(f"{unauth_server_certificate_base}/{certificate_common_name[0]}-license/v1/application-certificate").content
@@ -13,8 +13,14 @@ def get_widevine_certificate():
 def initiate_content_recovery():
        headers = {}
 
-       raw_bytes = requests.get('https://audio-fa.scdn.co/audio/7224408a3f0264b85f31d477618db65c9df27058?1650241870_oKSDqx8mBpg2fURrCYcEyt7CqI-ZUTulzZD-e8BjCjE=', headers)
-       print(f"Content bytes :: {raw_bytes.headers['Content-Range']}")
+       raw_bytes = requests.get('https://audio-fa.scdn.co/audio/7224408a3f0264b85f31d477618db65c9df27058?1650241870_oKSDqx8mBpg2fURrCYcEyt7CqI-ZUTulzZD-e8BjCjE=', headers=headers)
+       try:
+              print(f"Content bytes :: {raw_bytes.headers['Content-Range']}")
+       except:
+              print('Could not find content range (not sent in header response?)')
+       print(raw_bytes.content)
        sd.play(raw_bytes.content, 44100)
 
 print(f"G0t certificate :: {get_widevine_certificate()}" + ('\n'*3))
+
+initiate_content_recovery()
